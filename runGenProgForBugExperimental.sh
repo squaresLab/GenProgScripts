b35362be46a617d0179c9bce4b265dbea402f385
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2018-07-16 14:42:50 -0400

    oops, wrong runGenProgForBugExperimental.sh file

--- runGenProgForBugExperimental.sh
+++ runGenProgForBugExperimental.sh
@@ -1,247 +1,168 @@
 #!/bin/bash
 
-#The purpose of this script is to set up the environment to run Genprog of a particular defects4j bug.
+#The purpose of this script is to run Genprog of a particular defects4j bug.
 
 #Preconditions:
 #The variable D4J_HOME should be directed to the folder where defects4j is installed.
 #The variable GP4J_HOME should be directed to the folder where genprog4java is installed.
+#The variable DAIKONDIR should be directed to the folder where daikon is installed
 
 #Output
-#The output is a txt file with the output of running the coverage analysis of the test suite on each of the folders indicated.
+#The output is a folder created in the $D4J_HOME/5thParameter/ where all the variants are stored including the patch, if any was found.
 
-# 1st param: project name, sentence case (ex: Lang, Chart, Closure, Math, Time)
-# 2nd param: bug number (ex: 1,2,3,4,...)
-# 3th param: testing option (ex: humanMade, generated)
-# 4th param: test suite sample size (ex: 1, 100)
+#Parameters:
+# 1st param is the project in upper case (ex: Lang, Chart, Closure, Math, Time)
+# 2nd param is the bug number (ex: 1,2,3,4,...)
+# 3th param is the option of running the test suite (ex: allHuman, oneHuman, oneGenerated)
+# 4th param is the test suite sample size (ex: 1, 100)
 # 5th param is the folder where the bug files will be cloned to. Starting from $D4J_HOME (Ex: ExamplesCheckedOut)
-# 6th param is the folder where the java 7 instalation is located
-# 7th param is the folder where the java 8 instalation is located
+# 6th param is the initial seed. It will then increase the seeds by adding 1 until it gets to the number in the next param.
+# 7th param is the final seed.
+# 8th param is on if the purpose is to test only fault loc and not really trying to find a patch. When it has reached the end of fault localization it will stop.
+# 9th param is the folder where the java 7 instalation is located
+# 10th param is the folder where the java 8 instalation is located
+# 11th param is set to \"true\" if negative tests are to be specified using sampled tests else set this to \"false\""
+# 12th param is the path to file containing sampled negative tests"
+# 13th param is set to \"true\" if positive tests are to be specified using sampled tests else set this to \"false\""
+# 14th param is the path to file containing sampled positive tests"
+# 15th param is the path to the directory containing the class files of the tests relative to the path to the defects4j bug
 
-# Example usage, VM:
-#./prepareBug.sh Math 2 allHuman 100 ExamplesCheckedOut /usr/lib/jvm/java-7-oracle/ /usr/lib/jvm/java-8-oracle/ true <path to neg.test> true <path to pos.test>
 
-if [ "$#" -ne 12 ]; then
-    echo "This script should be run with 12 parameters:"
-	echo "1st param: project name, sentence case (ex: Lang, Chart, Closure, Math, Time)"
-	echo "2nd param: bug number (ex: 1,2,3,4,...)"
-	echo "3th param: testing option (ex: humanMade, generated)"
-	echo "4th param: test suite sample size (ex: 1, 100)"
-	echo "5th param is the folder where the bug files will be cloned to. Starting from $D4J_HOME"
-	echo "6th param is the folder where the java 7 instalation is located"
-	echo "7th param is the folder where the java 8 instalation is located"
-	echo "8th param is set to \"true\" if negative tests are to be specified using sampled tests else set this to \"false\""
-	echo "9th param is the path to file containing sampled negative tests"
-	echo "10th param is set to \"true\" if positive tests are to be specified using sampled tests else set this to \"false\""
-	echo "11th param is the path to file containing sampled positive tests"
-	echo "12th param is the path to the directory containing the class files of the tests relative to the path to the defects4j bug"
-    exit 0
-fi
+
+#Example of usage:
+#./runGenProgForBug.sh Math 2 allHuman 100 ExamplesCheckedOut 1 5 false /usr/lib/jvm/java-1.7.0-openjdk-amd64 /usr/lib/jvm/java-1.8.0-openjdk-amd64 false \"\" false \"\"
+
+if [ "$#" -ne 15 ]; then
+	echo "This script should be run with 15 parameters:"
+	echo " 1st param is the project in upper case (ex: Lang, Chart, Closure, Math, Time)"
+	echo " 2nd param is the bug number (ex: 1,2,3,4,...)"
+	echo " 3th param is the option of running the test suite (ex: allHuman, oneHuman, oneGenerated)"
+	echo " 4th param is the test suite sample size (ex: 1, 100)"
+	echo " 5th param is the folder where the bug files will be cloned to. Starting from $D4J_HOME (Ex: ExamplesCheckedOut)"
+	echo " 6th param is the initial seed. It will then increase the seeds by adding 1 until it gets to the number in the next param."
+	echo " 7th param is the final seed."
+	echo " 8th param is on if the purpose is to test only fault loc and not really trying to find a patch. When it has reached the end of fault localization it will stop."
+	echo " 9th param is the folder where the java 7 instalation is located"
+	echo " 10th param is the folder where the java 8 instalation is located"
+        echo " 11th param is set to \"true\" if negative tests are to be specified using sampled tests else set this to \"false\""
+        echo " 12th param is the path to file containing sampled negative tests"
+        echo " 13th param is set to \"true\" if positive tests are to be specified using sampled tests else set this to \"false\""
+        echo " 14th param is the path to file containing sampled positive tests"
+	echo " 15th param is the path to the directory containing the class files of the tests relative to the path to the defects4j bug"
+else
 
 PROJECT="$1"
 BUGNUMBER="$2"
 OPTION="$3"
 TESTSUITESAMPLE="$4"
 BUGSFOLDER="$5"
-DIROFJAVA7="$6"
-DIROFJAVA8="$7"
-SAMPLENEGTESTS="$8"
-NEGTESTPATH="$9"
-SAMPLEPOSTESTS="${10}"
-POSTESTPATH="${11}"
-TESTFOLDER="${12}"
+STARTSEED="$6"
+UNTILSEED="$7"
+JUSTTESTINGFAULTLOC="$8"
+DIROFJAVA7="/usr/lib/jvm/java-1.7.0-openjdk-amd64"
+DIROFJAVA8="/usr/lib/jvm/java-1.8.0-openjdk-amd64"
+SAMPLENEGTESTS="${11}"
+NEGTESTPATH="${12}"
+SAMPLEPOSTESTS="${13}"
+POSTESTPATH="${14}"
+TESTFOLDER="${15}"
 
-BASEDIR=$PWD
+
+#if [ "$#" -eq 15 ]; then
+  DIROFJAVA7="$9"
+  DIROFJAVA8="${10}"
+#fi
+
+
+#This transforms the first parameter to lower case. Ex: lang, chart, closure, math or time
+LOWERCASEPACKAGE=`echo $PROJECT | tr '[:upper:]' '[:lower:]'`
 
 #Add the path of defects4j so the defects4j's commands run
-export PATH=$PATH:"$D4J_HOME"/framework/bin/
-export PATH=$PATH:"$D4J_HOME"/framework/util/
-export PATH=$PATH:"$D4J_HOME"/major/bin/
-
-#copy these files to the source control
-
-mkdir -p $D4J_HOME/$BUGSFOLDER
-
-LOWERCASEPACKAGE=`echo $PROJECT | tr '[:upper:]' '[:lower:]'`
+export PATH=$PATH:$D4J_HOME/framework/bin
 
 # directory with the checked out buggy project
 BUGWD=$D4J_HOME/$BUGSFOLDER"/"$LOWERCASEPACKAGE"$BUGNUMBER"Buggy
-
-MODIFIEDCLASSESLIST=posTestsRemoved.tests
-
-#Checkout the buggy and fixed versions of the code (latter to make second testsuite
-defects4j checkout -p $1 -v "$BUGNUMBER"b -w $BUGWD
-
-##defects4j checkout -p $1 -v "$BUGNUMBER"f -w $D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE"$2"Fixed
-
-#Zhen's preprocessing step: separate positive and negative test cases
-PREVLOCATION=$(pwd)
-cd $BASEDIR/moveNegativeTests
-sh moveNegativeTests.sh $BUGWD $BUGWD/neg.tests $BUGWD/$MODIFIEDCLASSESLIST javaparser_javaparser-core_target_classes
-cd $PREVLOCATION
-
-#Compile the both buggy and fixed code
-for dir in Buggy
-do
-    pushd $D4J_HOME/$BUGSFOLDER"/"$LOWERCASEPACKAGE$BUGNUMBER$dir
-    defects4j compile
-    popd
-done
-# Common genprog libs: junit test runner and the like
-CONFIGLIBS=$GP4J_HOME"/lib/junittestrunner.jar"
-#:"$GP4J_HOME"/lib/commons-io-1.4.jar:"$GP4J_HOME"/lib/junit-4.12.jar:"$GP4J_HOME"/lib/hamcrest-core-1.3.jar"
-
-cd $D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2Buggy/
-TESTWD=`defects4j export -p dir.src.tests`
-SRCFOLDER=`defects4j export -p dir.bin.classes`
-COMPILECP=`defects4j export -p cp.compile`
-TESTCP=`defects4j export -p cp.test`
-WD=`defects4j export -p dir.src.classes`
-cd $BUGWD/$WD
-
-#Create file to run defects4j compile
-
-FILE=$D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2Buggy/runCompile.sh
-/bin/cat <<EOM >$FILE
-#!/bin/bash
-export JAVA_HOME=$DIROFJAVA7
-export PATH="$DIROFJAVA7/bin/:$PATH"
-#sudo update-java-alternatives -s java-7-oracle
-cd $D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2Buggy/
-$D4J_HOME/framework/bin/defects4j compile
-if [[ \$? -ne 0 ]]; then
-      echo "error compiling defect"
-      exit 1
-fi
 export JAVA_HOME=$DIROFJAVA8
-export PATH="$DIROFJAVA8/bin/:$PATH"
-#sudo update-java-alternatives -s java-8-oracle
-EOM
+export JRE_HOME=$DIROFJAVA8/jre
+export PATH=$DIROFJAVA8/bin/:$PATH
+#sudo update-java-alternatives -s $DIROFJAVA8
 
-chmod 777 $D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2Buggy/runCompile.sh
+BASEDIR=$PWD
+
+#Compile Genprog and put the class files in /bin
+#Go to the GenProg folder
+echo "right before compiling gp4j"
+if [ -d "$GP4J_HOME" ]; then
+	echo "compiling gp4j"
+  cd "$GP4J_HOME"
+  mvn package
+  if [[ $? -ne 0 ]]; then
+      echo "error building GenProg; exiting"
+      exit 1
+  fi
+
+  export JAVA_HOME=$DIROFJAVA7
+  export JRE_HOME=$DIROFJAVA7/jre
+  export PATH=$DIROFJAVA7/bin/:$PATH
+  #sudo update-java-alternatives -s $DIROFJAVA7
+
+    cd $BASEDIR
+
+	./prepareBugExperimental.sh $PROJECT $BUGNUMBER $OPTION $TESTSUITESAMPLE $BUGSFOLDER $DIROFJAVA7 $DIROFJAVA8 $SAMPLENEGTESTS $NEGTESTPATH $SAMPLEPOSTESTS $POSTESTPATH ${15}
+
+    if [ -d "$BUGWD/$WD" ]; then
+	echo "Going to BUGWD"
+      #Go to the working directory
+      cd $BUGWD/$WD
+
+      for (( seed=$STARTSEED; seed<=$UNTILSEED; seed++ ))
+      do
+	echo "RUNNING THE BUG: $PROJECT $BUGNUMBER, WITH THE SEED: $seed"
+
+	#Running until fault loc only
+	if [ $JUSTTESTINGFAULTLOC == "true" ]; then
+	  echo "justTestingFaultLoc = true" >> $D4J_HOME/$BUGSFOLDER/"$LOWERCASEPACKAGE""$BUGNUMBER"Buggy/defects4j.config
+	fi
+
+	#Changing the seed
+	CHANGESEEDCOMMAND="sed -i '1s/.*/seed = $seed/' "$D4J_HOME/$BUGSFOLDER/"$LOWERCASEPACKAGE""$BUGNUMBER"Buggy/defects4j.config
+	eval $CHANGESEEDCOMMAND
+
+	if [ $seed != $STARTSEED ]; then
+	  REMOVESANITYCOMMAND="sed -i 's/sanity = yes/sanity = no/' "$D4J_HOME/$BUGSFOLDER/"$LOWERCASEPACKAGE""$BUGNUMBER"Buggy/defects4j.config
+	  eval $REMOVESANITYCOMMAND
+
+	  REMOVEREGENPATHS="sed -i '/regenPaths/d' "$D4J_HOME/$BUGSFOLDER/"$LOWERCASEPACKAGE""$BUGNUMBER"Buggy/defects4j.config
+	  eval $REMOVEREGENPATHS
+	fi
+
+	export JAVA_HOME=$DIROFJAVA8
+	export JRE_HOME=$DIROFJAVA8/jre
+  	export PATH=$DIROFJAVA8/bin/:$PATH
+	#sudo update-java-alternatives -s $DIROFJAVA8
+
+	JAVALOCATION=$(which java)
+	$JAVALOCATION -ea -Dlog4j.configurationFile=file:"$GP4J_HOME"/src/log4j.properties -Dfile.encoding=UTF-8 -classpath "$GP4J_HOME"/target/uber-GenProg4Java-0.0.1-SNAPSHOT.jar clegoues.genprog4java.main.Main $GP4J_HOME $DIROFJAVA8 $DAIKONDIR $D4J_HOME/$BUGSFOLDER/"$LOWERCASEPACKAGE""$BUGNUMBER"Buggy/defects4j.config | tee $D4J_HOME/$BUGSFOLDER/"$LOWERCASEPACKAGE""$BUGNUMBER"Buggy/log"$PROJECT""$BUGNUMBER"Seed$seed.txt
 
 
-cd $BUGWD
+	#Save the variants in a tar file
+	#tar -cvf $D4J_HOME/$BUGSFOLDER/"$LOWERCASEPACKAGE""$BUGNUMBER"Buggy/variants"$PROJECT""$BUGNUMBER"Seed$seed.tar $D4J_HOME/$BUGSFOLDER/"$LOWERCASEPACKAGE""$BUGNUMBER"Buggy/tmp/
+	#mv $D4J_HOME/$BUGSFOLDER/"$LOWERCASEPACKAGE""$BUGNUMBER"Buggy/tmp/original/ $D4J_HOME/$BUGSFOLDER/"$LOWERCASEPACKAGE""$BUGNUMBER"Buggy/
+	#rm -r $D4J_HOME/$BUGSFOLDER/"$LOWERCASEPACKAGE""$BUGNUMBER"Buggy/tmp/
+	#mkdir $D4J_HOME/$BUGSFOLDER/"$LOWERCASEPACKAGE""$BUGNUMBER"Buggy/tmp/
+	#mv $D4J_HOME/$BUGSFOLDER/"$LOWERCASEPACKAGE""$BUGNUMBER"Buggy/original/ $D4J_HOME/$BUGSFOLDER/"$LOWERCASEPACKAGE""$BUGNUMBER"Buggy/tmp/
 
-#line for debugging
-#echo "$D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2Buggy/${12}"
-#exit 0
-
-#Create config file
-FILE=$D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2Buggy/defects4j.config
-/bin/cat <<EOM >$FILE
-seed = 0
-sanity = yes
-popsize = 20
-generations = 10
-javaVM = $DIROFJAVA7/jre/bin/java
-workingDir = $D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2Buggy/
-outputDir = $D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2Buggy/tmp
-classSourceFolder = $D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2Buggy/$SRCFOLDER
-classTestFolder = $D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2Buggy/${12}
-libs = $CONFIGLIBS
-sourceDir = $WD
-positiveTests = $D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2Buggy/pos.tests
-negativeTests = $D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2Buggy/neg.tests
-positiveTestClassesDaikonSample = $D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2Buggy/$MODIFIEDCLASSESLIST
-jacocoPath = $GP4J_HOME/lib/jacocoagent.jar
-testClassPath=$TESTCP
-srcClassPath=$COMPILECP
-compileCommand = $D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2Buggy/runCompile.sh
-targetClassName = $BUGWD/bugfiles.txt
-
-# 0.1 for GenProg and 1.0 for TrpAutoRepair and PAR
-sample=0.1
-
-# edits for PAR, GenProg, TrpAutoRepair
-#edits=append;replace;delete;FUNREP;PARREP;PARADD;PARREM;EXPREP;EXPADD;EXPREM;NULLCHECK;OBJINIT;RANGECHECK;SIZECHECK;CASTCHECK;LBOUNDSET;UBOUNDSET;OFFBYONE;SEQEXCH;CASTERMUT;CASTEEMUT
-edits=append;replace;delete
-#edits=FUNREP;PARREP;PARADD;PARREM;EXPREP;EXPADD;EXPREM;NULLCHECK;OBJINIT;RANGECHECK;SIZECHECK;CASTCHECK
-
-# don't know whats this used for. Ask Mau.
-#model=probabilistic
-#modelPath=/home/mausoto/probGenProg/genprog4java/overallModel.txt
-
-# use 1.0,0.1 for TrpAutoRepair and PAR. Use 0.65 and 0.35 for GenProg
-negativePathWeight=0.65
-positivePathWeight=0.35
-
-# trp for TrpAutoRepair, gp for GenProg and PAR
-#search=trp
-
-# used only for TrpAutoRepair. value=400
-maxVariants=400
-
-
-EOM
-
-#  get passing and failing tests as well as files
-#info about the bug
-
-if [ "$SAMPLENEGTESTS" = "true" ]; then
-        if [ "$NEGTESTPATH" = "" ]; then
-                echo "please enter path to file containing negative test cases"
-                exit 1
-        fi
-        cp $NEGTESTPATH $BUGWD/neg.tests
-#else
-  #not used due to Zhen's preprocessing step, which separates failing test cases into its own class(es)
-	#defects4j export -p tests.trigger > $BUGWD/neg.tests
+      done
+    fi
 fi
 
-currentpid="$currentpid $!"
-wait $currentpid
+fi #correct number of params
 
-case "$OPTION" in
-"humanMade" )
-	if [ "$SAMPLEPOSTESTS" = "true" ]; then
-        	if [ "$POSTESTPATH" = "" ]; then
-                	echo "please enter path to file containing positive test cases"
-  	   		exit 1
-        	fi
-	        cp $POSTESTPATH $BUGWD/pos.tests
-	else
-	     	defects4j export -p tests.all > $BUGWD/pos.tests
-	fi
-;;
-"allHuman" )
-	if [ "$SAMPLEPOSTESTS" = "true" ]; then
-        	if [ "$POSTESTPATH" = "" ]; then
-                	echo "please enter path to file containing positive test cases"
-  	   		exit 1
-        	fi
-	        cp $POSTESTPATH  $BUGWD/pos.tests
-	else
-		defects4j export -p tests.all > $BUGWD/pos.tests
-	fi
-;;
-"onlyRelevant" )
-	if [ "$SAMPLEPOSTESTS" = "true" ]; then
-        	if [ "$POSTESTPATH" = "" ]; then
-                	echo "please enter path to file containing positive test cases"
-  	   		exit 1
-        	fi
-	        cp $POSTESTPATH  $BUGWD/pos.tests
-	else
-		defects4j export -p tests.relevant > $BUGWD/pos.tests
-        fi
-;;
-esac
-
-currentpid="$currentpid $!"
-wait $currentpid
-
-#Remove a percentage of the positive tests in the test suite
-cd $D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2Buggy/
-
-if [[ $TESTSUITESAMPLE -ne 100 ]]
-then
-    PERCENTAGETOREMOVE=$(echo "$TESTSUITESAMPLE * 0.01" | bc -l )
-    echo "sample = $PERCENTAGETOREMOVE" >> $D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2Buggy/defects4j.config
-fi
-
-# get the class names to be repaired
-
-defects4j export -p classes.modified > $BUGWD/bugfiles.txt
-
-echo "This is the working directory: "
-echo $D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2Buggy/$WD
+#For use only of the Probabilistic vs Equally distributed comparisson
+#WHICHMODE="RegAllMut"
+#mv $D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2Buggy/ $D4J_HOME/$BUGSFOLDER/"ResultsRegvsProbMultiLine"/$LOWERCASEPACKAGE$2$WHICHMODE/
+#cd $GP4J_HOME/defects4j-scripts/
+#GETLASTLINESFROMLOGS="./probVsEDSpecificScripts/getLastLinesFromAllLogs.sh $PROJECT $BUGNUMBER $D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2$WHICHMODE/"
+#eval $GETLASTLINESFROMLOGS
+#CONVERTTOCSV="./probVsEDSpecificScripts/convertToCSVtheLastLines.sh $D4J_HOME/$BUGSFOLDER/$LOWERCASEPACKAGE$2$WHICHMODE/"
+#eval $CONVERTTOCSV
