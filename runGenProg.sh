c4dfc37e3fc636f5a9cc55200f6f2cd25d5cbad1
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2018-07-18 14:19:58 -0400

    added support for a invariantCheckerMode configuration

--- runGenProg.sh
+++ runGenProg.sh
@@ -8,7 +8,7 @@ P3=allHuman
 P4=20
 P5=BuggyGP4J
 P6=0
-P7=4
+P7=0
 P8=false
 P9=/Library/Java/JavaVirtualMachines/jdk1.8.0_172.jdk/Contents/Home
 P10=/Library/Java/JavaVirtualMachines/jdk1.8.0_172.jdk/Contents/Home
@@ -19,6 +19,7 @@ P14=\"\"
 P15=target/test-classes/
 P16=300000
 #5 minutes, needs to be long for Daikon
+P17=3
 
 cd GenProgScripts
-sh runGenProgForBugExperimental.sh $P1 $P2 $P3 $P4 $P5 $P6 $P7 $P8 $P9 $P10 $P11 $P12 $P13 $P14 $P15 $P16
+sh runGenProgForBugExperimental.sh $P1 $P2 $P3 $P4 $P5 $P6 $P7 $P8 $P9 $P10 $P11 $P12 $P13 $P14 $P15 $P16 $P17
