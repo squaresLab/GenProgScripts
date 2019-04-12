af9c4bfaa476b5d26c4f174e0aef6b897e01870c
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2019-04-12 16:25:44 -0400

    make helper script docker-compatible

--- runGenProg.sh
+++ runGenProg.sh
@@ -1,7 +1,3 @@
-export D4J_HOME="/home/ylyu1/genprog/defects4j"
-export GP4J_HOME="/home/ylyu1/genprog/genprog4java"
-export DAIKONDIR="/home/ylyu1/genprog/daikon-5.6.6"
-
 P1=$1
 P2=$2
 P3=allHuman
@@ -10,8 +6,8 @@ P5=BuggyGP4J$3
 P6=0
 P7=4
 P8=false
-P9=/usr/lib/jvm/java-8-openjdk-amd64
-P10=/usr/lib/jvm/java-8-openjdk-amd64
+P9=$JAVA7_HOME
+P10=$JAVA_HOME
 P11="false"
 P12=\"\"
 P13="false"
@@ -19,7 +15,7 @@ P14=\"\"
 P15=target/test-classes/
 P16=300000
 #5 minutes, needs to be long for Daikon
-P17=$3
+P17=0
 
 #cd GenProgScripts
 sh runGenProgForBugExperimental.sh $P1 $P2 $P3 $P4 $P5 $P6 $P7 $P8 $P9 $P10 $P11 $P12 $P13 $P14 $P15 $P16 $P17
