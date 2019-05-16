b40be94c723d5dd517561e8e8d84505a6cf8e549
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2019-05-15 20:44:24 -0400

    mode not needed for mut test

--- runGenProg.sh
+++ runGenProg.sh
@@ -1,12 +1,11 @@
 #1st param: project name (Math, Lang, ...)
 #2nd param: bug number
-#3rd param: mode
 
 P1=$1
 P2=$2
 P3=allHuman
 P4=20
-P5=BuggyGP4J$3
+P5=BuggyGP4J
 P6=0
 P7=4
 P8=false
@@ -19,7 +18,7 @@ P14=\"\"
 P15=target/test-classes/
 P16=300000
 #5 minutes, needs to be long for Daikon
-P17=0
+P17=0 #deprecated, not needed for mutation testing
 
 #cd GenProgScripts
 sh runGenProgForBugExperimental.sh $P1 $P2 $P3 $P4 $P5 $P6 $P7 $P8 $P9 $P10 $P11 $P12 $P13 $P14 $P15 $P16 $P17
